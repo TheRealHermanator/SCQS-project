0bcbb227be90c5ce02ba5f22d65572cff07ae95d
Jarid <jarid.piceu@ugent.be>
2025-05-11 17:01:20 +0200

    Update better plots

--- 6VertexModel_Jonas/PlotSingularValuesPhase.jl
+++ 6VertexModel_Jonas/PlotSingularValuesPhase.jl
@@ -6,42 +6,66 @@ include("6VertexModel.jl")
 include("ModifiedTRGflow.jl")
 
 # Variables
-nmaxiter = 25
-select_iter = 21
-as = LinRange(0.1, 1.0, 10)
+nmaxiter = 15
+as_1 = LinRange(0.1, 0.5, 5)
+as_2 = LinRange(0.6, 1.0, 5)
 b = 1.5 #such that we have a phase transition for a = 0.5 (delta = 1)
 x = Float64[]
 y = Float64[]
 
-function compute_BTRG_data_sing(ac_val, bc_val; ntruncdim=16, nmaxiter=nmaxiter)
+function compute_TRG_data_sing(ac_val, bc_val; ntruncdim=16, nmaxiter=nmaxiter)
     T = six_vertex_tensor(ac_val, bc_val, 1)  # sixvertex(a=ac_val, b=bc_val, c=1) # six_vertex_tensor(ac_val, bc_val, 1)
     scheme = BTRG(T)
     data, sing = run_sing!(scheme, truncdim(ntruncdim), maxiter(nmaxiter); verbosity=1)
     return data, sing
 end
-# Iterate over different a's
-for a in as
-    data, sing = compute_BTRG_data_sing(a, b; ntruncdim=16, nmaxiter=nmaxiter)
-    svals = sing[select_iter]
-    maxval = maximum(svals)
-    for val in svals
-        push!(x, a)              # Store the iteration index
-        push!(y, val / maxval)            # Store the singular value
-    end
-end
+
 
 # Scatter plot
-plt = scatter(x, y,
-    xlabel = "a",
+plt = scatter(
+    xlabel = "\$a\$",
     ylabel = "Singular Values",
-    title = "Singular Values In Different Phases (Iteration: $select_iter)",
+    title = "Singular Values In Different Phases at RG step $nmaxiter",
     yscale = :log10,
     legend = false,
     )
+
+# Plotting the singular values for different a values
+# First the ones with delta > 1 are plotted since it's easier to see the degeneracies
+for a in as_1
+    data, sing = compute_TRG_data_sing(a, b; ntruncdim=16, nmaxiter=nmaxiter)
+    svals = sing[nmaxiter]
+    maxval = maximum(svals)
+    deg = 0
+    for val in svals
+        if abs(val-maxval)<1e-5
+            deg +=1
+        else
+            scatter!((a, val/maxval), markercolor=:grey)
+        end
+        
+    end
+    if deg>1
+        scatter!((a, 1), markercolor=:blue, markershape=:star5)
+        annotate!((a-0.05, 1-0.2), "$deg")
+    else
+        scatter!((a, 1), markercolor=:blue)
+    end
+end
+# Then the ones with delta < 1, we don't have to worry about degeneracies here
+for a in as_2
+    data, sing = compute_TRG_data_sing(a, b; ntruncdim=16, nmaxiter=nmaxiter)
+    svals = sing[nmaxiter]
+    maxval = maximum(svals)
+
+    for val in svals
+       scatter!((a, val/maxval), markercolor =:grey) 
+    end
+end
 line = LinRange(5e-3, 1.1e+0, 1000)
 plot!(0.5 * ones(1000), line, color = :red)
-annotate!(0.3, 1.5, "∆ > 1")
-annotate!(0.5, 1.5, "∆ = 1")
-annotate!(0.8, 1.5, "∆ < 1")
+annotate!(0.3, 1.5, "\$∆ > 1\$")
+annotate!(0.5, 1.5, "\$∆ = 1\$")
+annotate!(0.8, 1.5, "\$∆ < 1\$")
 ylims!(5e-3, 2e+0)
-savefig(plt, "SVs delta Iter$select_iter")
+savefig(plt, "SVs delta")
